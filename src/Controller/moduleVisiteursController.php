c95a18d718098d9996503b19773e1dfa4ea5d226
salaniko <salaunico@yahoo.fr>
2020-03-11 16:37:16 +0100

    Code à corriger

--- src/Controller/moduleVisiteursController.php
+++ src/Controller/moduleVisiteursController.php
@@ -8,11 +8,12 @@
     use Symfony\Component\HttpFoundation\JsonResponse;
 
     use App\Entity\Region;
+    use App\Entity\Profil;
     use App\Entity\VisiteurRegion;
     use App\Entity\Specialite;
     use App\Entity\Praticiens;
     use App\Entity\PraticiensRegion;
-    use App\Entity\Visiteur;
+    use App\Entity\Visite;
 	
 	/**
     * @Route("/gestionVisite")
@@ -25,11 +26,33 @@
          */
     	public function gestionVisite()
     	{
-    		$html = $this->render('moduleVisiteurs/moduleVisiteurs.html.twig', array(
-    			'title' => 'Gestion des visites'
-    		));
+            $user = $this->getUser();
 
-    		return $html;
+            $em = $this->getDoctrine()->getManager();
+
+            if ($user->hasRole('ROLE_RESPONSABLE'))
+            {
+                $listeRegions =  $em->getRepository(Region::class)->getRegionsByResponsable($user->getId());
+                $listeVisiteurs = $em->getRepository(VisiteurRegion::class)->findBy(array('regCode' => array_column($listeRegions , "regCode") , 'active' => 1));
+                $listeNomsVisiteurs = $em->getRepository(Profil::class)->findBy(array('id' => array_column($listeVisiteurs, "matricule")));
+
+                $html = $this->render('moduleVisiteurs/moduleVisiteurs.html.twig', array(
+                    'title' => 'Gestion des visites',
+                    'listeRegions' => $listeRegions,
+                    'listeVisiteurs' => $listeVisiteurs,
+                    'listeNomsVisiteurs' => $listeNomsVisiteurs
+                ));
+
+                return $html;
+
+            } else 
+            {
+                $html = $this->render('moduleVisiteurs/moduleVisiteurs.html.twig', array(
+                    'title' => 'Gestion des visites'
+                ));
+
+                return $html;
+            }
     	}
 
         /**
@@ -37,7 +60,29 @@
          */
         public function creationVisite()
         {
-            return $this->redirectToRoute('gestionVisite');
+            $user = $this->getUser();
+
+            if($user->hasRole('ROLE_RESPONSABLE'))
+            {
+                echo "toto";
+                $entityManager = $this->getDoctrine()->getManager();
+                $data = $request->request;
+
+                $visite = new Visite();
+
+                $visite->setIdPraticien($data->get('id_praticien'));
+                $visite->setMatricule($data->get('matricule'));
+                $visite->setDateVisite($data->get('date_visite'));
+                $visite->setMotif($data->get('motif'));
+                $visite->setConvaincu($data->get('convaincu'));
+                $visite->setVisitePlanifiee($data->get('contre_visite'));
+
+                $entityManager->persist($visite);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('gestionVisite');
+            }
+            
         }
     }
 
